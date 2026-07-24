98ab2dbb0832c2fa9a1ce31d2d72481dad83aa08
L. Felipe G. Fernandes <70975952+EuLipao@users.noreply.github.com>
2026-07-24 03:24:08 +0000

    feito menu para jogar ou sair, feito entrada de dados para escolher onde quer posicionar Navio 1 e Navio 2

--- batalhaNavalTeste.c
+++ batalhaNavalTeste.c
@@ -5,25 +5,24 @@ int main(){
     int tabuleiro [10][10];
     int i,j;
     int indiceTabuleiro = 1;
-    int posicaoI,sentido;
-    char posicaoJ;
-    //tabuleiro recebe 0 em todas posições 
-    for(i=0;i < 10;i++){
-        for(j=0;j < 10; j++){
-            tabuleiro [i][j]= 0;
-            }
-    printf("\n");
-    }
-    
+    int posicaoI1,posicaoI2,sentido1,sentido2;
+    char posicaoJ1,posicaoJ2;
+    int menu,reiniciar;
+do{ 
     printf("*********************************\n");
     printf("****B A T A L H A  N A V A L ****\n");
     printf("*********************************\n");
-    printf("                  _______        \n");
+    printf("                      //         \n");
+    printf("                  ___| |_        \n");
     printf("            ______|_____|        \n");
     printf("     _______|___________|_____   \n");
     printf("    |   o       o       o   /    \n");
     printf("~~~~|______________________/~~~~~\n");
     printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+
+
+
+   
     
     // fazer menu 1 jogar, 2 instruções, 3 sair
     printf("\n");
@@ -31,11 +30,21 @@ int main(){
     printf("2. Instruções ");
     printf("3. Sair *");
     printf("\n");
+    scanf("%d",&menu);
+switch (menu)
+{
+case 1:
+    //tabuleiro recebe 0 em todas posições 
+    for(i=0;i < 10;i++){
+        for(j=0;j < 10; j++){
+            tabuleiro [i][j]= 0;
+            }
+      }
     
     //exibe tabuleiro
     printf("\n");
     printf("     A  B  C  D  E  F  G  H  I  J\n");
-    for(i=0;i < 10;i++){    
+    for(i=0,indiceTabuleiro=1;i < 10;i++){    
        printf(" %.2d ",indiceTabuleiro);       
        for(j=0;j < 10; j++){           
             printf(" %d ",tabuleiro [i][j]);            
@@ -46,62 +55,64 @@ int main(){
         
     printf("\n");
     
-    //posição numero
-    //verifica posição do numero
-    printf("Em qual Numero quer posicionar 1 Navio? \n");
-    scanf("%d",&posicaoI);
-    if(posicaoI >10){
+        //verifica posição do numero navio 1
+    printf("Em qual Numero quer posicionar Navio 1? \n");
+    scanf("%d",&posicaoI1);
+    if(posicaoI1 >10){
         printf("Posição invalida!\n");
-    }
+    }    
+    --posicaoI1;
 
-    // posição letra
-    printf("Em qual Letra quer posicionar 1 Navio? \n");
-    scanf(" %c",&posicaoJ);
+
+    // verifica posição letra
+    printf("Em qual Letra quer posicionar Navio 1? \n");
+    scanf(" %c",&posicaoJ1);
+
     // posição vertical ou horizontal
     printf("Quer posicionar na vertical(1) ou na horizontal(2) ?\n");
-        scanf("%d",&sentido);
+        scanf("%d",&sentido1);
 
-        //verifica posição da letra
-       switch (posicaoJ){
+        //verifica posição da letra index navio 1
+       switch (posicaoJ1){
         case 'a':
         case 'A':
-             posicaoJ = 0;
+             posicaoJ1 = 0;
         break;
         case 'b':
         case 'B':
-             posicaoJ = 1;
+             posicaoJ1 = 1;
         break;
         case 'c':
         case 'C':
-           posicaoJ = 2;
+           posicaoJ1 = 2;
         break;
         case 'd':
         case 'D':
-           posicaoJ = 3;
+           posicaoJ1 = 3;
         break;
         case 'e':
         case 'E':
-           posicaoJ = 4;
+           posicaoJ1 = 4;
         break;
         case 'f':
         case 'F':
-           posicaoJ = 5;
+           posicaoJ1 = 5;
         break;
         case 'g':
         case 'G':
-           posicaoJ = 6;
+           posicaoJ1 = 6;
         break;
         case 'h':
         case 'H':
-           posicaoJ = 7;
+           posicaoJ1 = 7;
         break;
         case 'i':
         case 'I':
-           posicaoJ = 8;
+           posicaoJ1 = 8;
         break;
         case 'j':
         case 'J':
-           posicaoJ = 9;
+           posicaoJ1 = 9;
         break;
         
     default:
@@ -109,10 +120,146 @@ int main(){
     break;
     } 
     
-    printf("index numero(linha) %d \n",--posicaoI);
-    printf("index letra(coluna) %d \n",posicaoJ);
-    printf("sentido que vai ser %s",sentido == 1 ? "Vertical" : "Horizontal");
+    //NAVIO 2
+
+    //verifica posição do numero navio 2
+    printf("Em qual Numero quer posicionar Navio 2? \n");
+    scanf("%d",&posicaoI2);
+    if(posicaoI2 >10){
+        printf("Posição invalida!\n");
+    }    
+    --posicaoI2;
+
+
+    // verifica posição letra
+    printf("Em qual Letra quer posicionar Navio 2? \n");
+    scanf(" %c",&posicaoJ2);
+
+    // posição vertical ou horizontal navio 2
+    printf("Quer posicionar na vertical(1) ou na horizontal(2) Navio 2 ?\n");
+        scanf("%d",&sentido2);
+
+        //verifica posição da letra index navio 2
+       switch (posicaoJ2){
+        case 'a':
+        case 'A':
+             posicaoJ2 = 0;
+        break;
+        case 'b':
+        case 'B':
+             posicaoJ2 = 1;
+        break;
+        case 'c':
+        case 'C':
+           posicaoJ2 = 2;
+        break;
+        case 'd':
+        case 'D':
+           posicaoJ2 = 3;
+        break;
+        case 'e':
+        case 'E':
+           posicaoJ2 = 4;
+        break;
+        case 'f':
+        case 'F':
+           posicaoJ2 = 5;
+        break;
+        case 'g':
+        case 'G':
+           posicaoJ2 = 6;
+        break;
+        case 'h':
+        case 'H':
+           posicaoJ2 = 7;
+        break;
+        case 'i':
+        case 'I':
+           posicaoJ2 = 8;
+        break;
+        case 'j':
+        case 'J':
+           posicaoJ2 = 9;
+        break;
+        
+    default:
+    printf("Posição invalida!\n");
+    break;
+    } 
     
+    /*
+    printf("index numero(linha) %d \n",posicaoI1);
+    printf("index letra(coluna) %d \n",posicaoJ1);
+    printf("sentido que vai ser %s",sentido1 == 1 ? "Vertical" : "Horizontal");
+    printf("index numero(linha) %d \n",posicaoI2);
+    printf("index letra(coluna) %d \n",posicaoJ2);
+    printf("sentido que vai ser %s",sentido2 == 1 ? "Vertical" : "Horizontal");*/
+   
+    //navio 1
+    if(sentido1 == 2){
+    for(i = 0; i < 3; i++){
+      tabuleiro[posicaoI1][posicaoJ1] = 3;
+      posicaoJ1++;
+      }
+    }else if(sentido1 == 1){
+      for(i = 0; i < 3; i++){
+      tabuleiro[posicaoI1][posicaoJ1] = 3;
+      posicaoI1++;
+      }
+
+    }
+    //navio 2
+    if(sentido2 == 2){
+    for(i = 0; i < 3; i++){
+      tabuleiro[posicaoI2][posicaoJ2] = 3;
+      posicaoJ2++;
+      }
+    }else if(sentido2 == 1){
+      for(i = 0; i < 3; i++){
+      tabuleiro[posicaoI2][posicaoJ2] = 3;
+      posicaoI2++;
+      }
+
+    }
+
+    //exibe tabuleiro atualizado
+    printf("\n");
+    printf("     A  B  C  D  E  F  G  H  I  J\n");
+    for(i=0,indiceTabuleiro=1;i < 10;i++){    
+       printf(" %.2d ",indiceTabuleiro);       
+       for(j=0;j < 10; j++){           
+            printf(" %d ",tabuleiro [i][j]);            
+            }
+          indiceTabuleiro++;
+            printf("\n");
+         }
+
+
+  printf("Ir para o menu? (1. Sim / 2. Não)\n");
+  scanf("%d",&reiniciar);
+
+
+      break;
+  
+
+   // regras
+   case 2:
+      printf("regras\n");
+   break;
+
+   case 3:
+   printf("jogo finalizando...\n");
+         break;
+   default:
+   break;
+
+}
+
+
+
+} while (reiniciar == 1);
+
+printf("Finalizado!\n");
     return 0;
     
 }
